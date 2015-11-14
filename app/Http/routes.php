6f590f339cc056980a7934695fa1fe01382eeeb0
Samson Tse <tse.samson@gmail.com>
2015-11-14 10:59:01 -0500

    Added debug route to check database

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Debug Route
+Route::get('/debug', function() {
+	echo '<pre>';
+
+	echo '<h1>Environment</h1>';
+	echo App::environment().'</h1>';
+
+	echo '<h1>Debugging?</h1>';
+	if(config('app.debug')) echo "Yes"; else echo "No";
+
+	echo '<h1>Database Config</h1>';
+
+	//Output Mysql credentials
+	echo '<h1>Test Database Connection</h1>';
+	try {
+		$result= DB::select('SHOW DATABASES;');
+		echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+		echo"<br/><br/>Your Databases:<br/><br/>";
+		print_r($result);
+	}
+	catch(Exception $e) {
+		echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+	}
+
+	echo '</pre>';
+});
